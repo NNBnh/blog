3d91bc421677a02ed874e701d7a04a6fdfff4054
NNB <nnbnh@protonmail.com>
2022-12-16 09:16:33 +0000

    modified:   .eleventy.js 	new file:   .eleventyignore 	modified:   package-lock.json 	modified:   package.json 	new file:   src/404.njk 	new file:   src/_includes/footer.njk 	new file:   src/_includes/layouts/base.njk 	deleted:    src/_includes/layouts/default.njk 	new file:   src/_includes/layouts/post.njk 	new file:   src/_includes/navbar.njk 	deleted:    src/index.md 	new file:   src/index.njk 	new file:   src/posts/demo.md 	modified:   tailwind.config.js

--- .eleventy.js
+++ .eleventy.js
@@ -2,7 +2,19 @@ const postcss = require('postcss');
 const tailwindcss = require('tailwindcss');
 const autoprefixer = require('autoprefixer');
 
+const markdownIt = require('markdown-it')
+const markdownItAnchor = require('markdown-it-anchor')
+
+const pluginTOC = require('eleventy-plugin-toc')
+const pluginRss = require("@11ty/eleventy-plugin-rss");
+const pluginSyntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
+
 module.exports = (eleventyConfig) => {
+  eleventyConfig.setLibrary(
+    'md',
+    markdownIt().use(markdownItAnchor)
+  )
+
   eleventyConfig.addNunjucksAsyncFilter('postcss', (cssCode, done) => {
     postcss([tailwindcss(require('./tailwind.config.js')), autoprefixer()])
       .process(cssCode)
@@ -12,11 +24,16 @@ module.exports = (eleventyConfig) => {
       );
   });
   eleventyConfig.addWatchTarget('styles/**/*.css');
+
+  eleventyConfig.addPlugin(pluginTOC)
+  eleventyConfig.addPlugin(pluginRss);
+  eleventyConfig.addPlugin(pluginSyntaxHighlight);
+
   return {
+    pathPrefix: '/blog/',
     dir: {
       input: 'src',
       output: 'dist',
     },
-    pathPrefix: '/blog/'
   };
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,7 @@
 module.exports = {
   content: ['./src/**/*.{njk,md}'],
   plugins: [require('daisyui'), require('@tailwindcss/typography')],
+  daisyui: {
+    themes: ['night'],
+  },
 };
