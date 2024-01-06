3ff2b464561fe9785daac04d4064cf258c943c51
NNB <nnbnh@protonmail.com>
2024-01-06 16:04:30 +0700

    Updated DaisyUI

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,16 +6,21 @@ module.exports = {
       {
         mytheme: {
           'primary': '#99d1db',
+          'primary-content': '#004955',
           'secondary': '#babbf1',
+          'secondary-content': '#000262',
           'accent': '#f2d5cf',
+          'accent-content': '#601707',
           'neutral': '#232634',
+          'neutral-content': '#c6d0f5',
           'base-100': '#303446',
           'base-200': '#292c3c',
           'base-300': '#232634',
+          'base-content': '#c6d0f5',
           'info': '#8caaee',
           'success': '#a6d189',
           'warning': '#ef9f76',
-          'error': '#ea999c'
+          'error': '#ea999c',
         },
       },
     ],
